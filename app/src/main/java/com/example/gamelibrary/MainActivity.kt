23aab524364aae031687e1f751d6c9bd3f5f1de0
polyc <ichigo69600@gmail.com>
2020-07-03 12:46:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gamelibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gamelibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
