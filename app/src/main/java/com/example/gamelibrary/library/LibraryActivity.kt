0ee808a1103c836a0b8a4e8ceb74aea9e268fbf0
polyc <ichigo69600@gmail.com>
2020-07-06 17:31:21 +0200

    base for library activity

---
+++ app/src/main/java/com/example/gamelibrary/library/LibraryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamelibrary.library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamelibrary.R
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
