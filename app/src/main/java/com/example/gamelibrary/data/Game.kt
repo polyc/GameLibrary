ea643e053fdd43a4764adec446bff95e51eb6fa2
polyc <ichigo69600@gmail.com>
2020-07-11 13:46:48 +0200

    prepare data class to host more fields

--- app/src/main/java/com/example/gamelibrary/data/Game.kt
+++ app/src/main/java/com/example/gamelibrary/data/Game.kt
@@ -1,9 +1,15 @@
 package com.example.gamelibrary.data
 
 data class Game(val name :String,
-                val backgroundImage :String?,
-                val metacriticRating :Int?,
-                val id :Int){
+                val id :Int,
+                val backgroundImage :String? = null,
+                val metacriticRating :Int? = null,
+                val description: String? = null,
+                val website: String? = null,
+                val releaseDate: String? = null,
+                val averagePlaytime: Int? = null,
+                val dominantColor: String? = null
+){
     override fun toString(): String {
         return name + " " + metacriticRating.toString()
     }
